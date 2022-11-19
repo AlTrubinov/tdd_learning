11df1798b42dcb7a0afdf0b742df1a5ac1b361de
Aleksey Trubinov <altrubinov26@gmail.com>
2022-11-19 16:24:13 +0700

    ФТ теперь проверяет возможность ввести элемент списка

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 import unittest
 
 
@@ -21,18 +24,34 @@ class NewVisitorTest(unittest.TestCase):
 
         # Пользователь видит заголовок и шапку страницы онлайн-приложения неотложных дел
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element(By.TAG_NAME,'h1').text
+        self.assertIn('To-Do', header_text)
 
         # Пользователю сразу же предлагается ввести элемент списка
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Пользователь набирает в текстовом поле "Купить павлиньи перья" (его хобби –
         # вязание рыболовных мушек)
+        inputbox.send_keys('Купить павлиньи перья')
 
         # Когда пользователь нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
 
         # Текстовое поле по-прежнему приглашает пользователя добавить еще один элемент.
         # Пользователь вводит "Сделать мушку из павлиньих перьев"
+        self.fail('Закончить тест!')
 
         # Страница снова обновляется, и теперь показывает оба элемента списка
 
@@ -43,6 +62,7 @@ class NewVisitorTest(unittest.TestCase):
         # Пользователь посещает этот URL-адрес – список по-прежнему там.
 
         # Пользователь выходит из браузера
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
