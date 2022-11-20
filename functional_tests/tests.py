f48f1eea1ba40e62b6389d835a0e7643afe15fe7
Aleksey Trubinov <altrubinov26@gmail.com>
2022-11-20 17:49:32 +0700

    Рефакторизация функции явного ожидания строки functional_tests в неявное ожидание с конструкцией try/except

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 from django.test import LiveServerTestCase
 import time
-import unittest
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -18,11 +20,19 @@ class NewVisitorTest(LiveServerTestCase):
         '''Выход из браузера'''
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        '''Подтверждение строки в таблице списка'''
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        '''Ожидание строки в таблице списка'''
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.3)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         '''Тест: можно начать список и получить его позже'''
@@ -31,11 +41,11 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Пользователь видит заголовок и шапку страницы онлайн-приложения неотложных дел
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME,'h1').text
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
 
         # Пользователю сразу же предлагается ввести элемент списка
-        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -48,20 +58,18 @@ class NewVisitorTest(LiveServerTestCase):
         # Когда пользователь нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
 
         # Текстовое поле по-прежнему приглашает пользователя добавить еще один элемент.
         # Пользователь вводит "Сделать мушку из павлиньих перьев"
-        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Сделать мушку из павлиньих перьев')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # Страница снова обновляется, и теперь показывает оба элемента списка
-        self.check_for_row_in_list_table('1: Купить павлиньи перья')
-        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+        self.wait_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 
         # Пользователю интересно, запомнит ли сайт список. Далее пользователь видит, что
         # сайт сгенерировал для него уникальный URL-адрес – об этом
@@ -71,6 +79,3 @@ class NewVisitorTest(LiveServerTestCase):
         # Пользователь посещает этот URL-адрес – список по-прежнему там.
 
         # Пользователь выходит из браузера
-
-
-
